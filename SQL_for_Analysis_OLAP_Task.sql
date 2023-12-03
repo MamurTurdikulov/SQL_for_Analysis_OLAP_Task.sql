087d757aba6aea485c646e72f9aa60e784bac77a
MamurTurdikulov <148514012+MamurTurdikulov@users.noreply.github.com>
2023-12-03 11:27:42 +0500

    Update SQL_for_Analysis_OLAP_Task.sql

--- SQL_for_Analysis_OLAP_Task.sql
+++ SQL_for_Analysis_OLAP_Task.sql
@@ -1,16 +1,16 @@
--- Retrieve the total sales amount for each product category for a specific time period
+-- Task 1: Retrieve the total sales amount for each product category for a specific time period
 SELECT ProductCategory, SUM(SalesAmount) AS TotalSales
 FROM YourSalesTable
 WHERE Date >= 'start_date' AND Date <= 'end_date'
 GROUP BY ProductCategory;
 
--- Calculate the average sales quantity by region for a particular product
+-- Task 2: Calculate the average sales quantity by region for a particular product
 SELECT Region, AVG(SalesQuantity) AS AvgSalesQuantity
 FROM YourSalesTable
 WHERE Product = 'your_product'
 GROUP BY Region;
 
--- Find the top five customers with the highest total sales amount
+-- Task 3: Find the top five customers with the highest total sales amount
 SELECT CustomerID, SUM(SalesAmount) AS TotalSales
 FROM YourSalesTable
 GROUP BY CustomerID
