5c3240a7443d30ebdb9f9343934c6ea37c00f4ff
MamurTurdikulov <148514012+MamurTurdikulov@users.noreply.github.com>
2023-12-03 17:57:26 +0000

    Update SQL_for_Analysis_OLAP_Task.sql

--- SQL_for_Analysis_OLAP_Task.sql
+++ SQL_for_Analysis_OLAP_Task.sql
@@ -1,18 +1,41 @@
 -- Task 1: Retrieve the total sales amount for each product category for a specific time period
-SELECT ProductCategory, SUM(SalesAmount) AS TotalSales
-FROM YourSalesTable
-WHERE Date >= 'start_date' AND Date <= 'end_date'
-GROUP BY ProductCategory;
+SELECT
+    pc.category_name,
+    SUM(s.sales_amount) AS total_sales_amount
+FROM
+    sales s
+    JOIN product p ON s.product_id = p.product_id
+    JOIN product_category pc ON p.category_id = pc.category_id
+WHERE
+    s.transaction_date BETWEEN 'start_date' AND 'end_date'
+GROUP BY
+    pc.category_name;
 
 -- Task 2: Calculate the average sales quantity by region for a particular product
-SELECT Region, AVG(SalesQuantity) AS AvgSalesQuantity
-FROM YourSalesTable
-WHERE Product = 'your_product'
-GROUP BY Region;
+SELECT
+    r.region_name,
+    AVG(s.sales_quantity) AS avg_sales_quantity
+FROM
+    sales s
+    JOIN store st ON s.store_id = st.store_id
+    JOIN region r ON st.region_id = r.region_id
+    JOIN product p ON s.product_id = p.product_id
+WHERE
+    p.product_name = 'your_product_name'
+GROUP BY
+    r.region_name;
+
 
 -- Task 3: Find the top five customers with the highest total sales amount
-SELECT CustomerID, SUM(SalesAmount) AS TotalSales
-FROM YourSalesTable
-GROUP BY CustomerID
-ORDER BY TotalSales DESC
+SELECT
+    c.customer_id,
+    c.customer_name,
+    SUM(s.sales_amount) AS total_sales_amount
+FROM
+    sales s
+    JOIN customer c ON s.customer_id = c.customer_id
+GROUP BY
+    c.customer_id, c.customer_name
+ORDER BY
+    total_sales_amount DESC
 LIMIT 5;
